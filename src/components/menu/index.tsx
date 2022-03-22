ac4fd4ada6f258c4085b56664d203c1cdecff2ef
Alexis Caballero <alexis.caballero@mercadolibre.com>
2022-03-22 00:45:23 -0300

    styles for the menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -4,10 +4,18 @@ import './index.scss';
 const HomeMenu = (props: HomeMenuProps) => {
     return (
         <Menu pageWrapId={props.pageWrapId} outerContainerId={props.outerContainerId} >
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a className="menu-item--small" href="">Settings</a>
+            <div>
+                <i className="fa fa-bookmark menu-icon"></i>
+                <a id="home" className="menu-item" href="/">Favourites</a>
+            </div>
+            <div>
+                <i className="fa fa-bookmark menu-icon"></i>
+                <a id="home" className="menu-item" href="/">TODO</a>
+            </div>
+            <div>
+                <i className="fa fa-bookmark menu-icon"></i>
+                <a id="home" className="menu-item" href="/">TODO</a>
+            </div>
         </Menu>)
 }
 
